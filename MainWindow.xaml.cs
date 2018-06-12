2166639bfc73665a8e5c2f9e957c351eca19f522
slpatrycja <sl.patrycja.of@gmail.com>
2018-06-12 13:39:09 +0200

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+
+namespace kalkulatorKcal
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Przelicznik przelicznik = new Przelicznik();
+            przelicznik.Ilosc = Int32.Parse(waga.Text);
+            przelicznik.UstawKalorycznosc(Int32.Parse(kalorie.Text));
+            przelicznik.UstawRodzaj(lista.SelectedItem.ToString());
+            poleWyniku.Text = "Zjadłeś/aś" + przelicznik.ZwrocRodzaj() + "o wadze" + przelicznik.Ilosc + "co daje" + przelicznik.Oblicz() + "kalorii.";
+        }
+    }
+}
---
+++ Przelicznik.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace kalkulatorKcal
+    {
+        class Przelicznik
+        {
+            private string rodzaj;
+            private int ilosc;
+            public int Ilosc
+            {
+                get
+                {
+                    return ilosc;
+                }
+
+                set
+                {
+                    ilosc = value;
+                }
+            }
+            private int kalorycznosc;
+
+            public void UstawRodzaj(string rodzaj)
+            {
+                this.rodzaj = rodzaj;
+            }
+
+            public string ZwrocRodzaj()
+            {   
+                  return rodzaj;
+            }
+
+            public void UstawKalorycznosc(int kalorycznosc)
+            {
+                this.kalorycznosc = kalorycznosc;
+            }
+
+            public int ZwrocKalorycznosc()
+            {
+                return kalorycznosc;
+            }
+
+            public int Oblicz()
+            {
+                int wynik;
+                wynik = (kalorycznosc * ilosc) / 100;
+            return wynik;
+            }
+        }
+
+    }
+
